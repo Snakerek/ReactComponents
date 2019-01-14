ec8b721b65aa579b55cf1a2c7f1f193fb49f64f1
Kacper Zieliński <zielinski.kac@gmail.com>
2019-01-14 15:44:16 +0100

    Ustawione struktórę podstawową komponentu komentarza

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-	return <div>Hello</div>;
+	return (
+		<div className="ui container comments">
+			<div className="comment">
+				<a href="#" className="avatar">
+					<img alt="avatar" />
+				</a>
+				<div className="content">
+					<a href="#" className="author">
+						Kacper
+					</a>
+					<div className="metadata">
+						<span className="date">Dzisiaj o 6:00</span>
+					</div>
+					<div className="text">Jakiś przykładowy wpis.</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
