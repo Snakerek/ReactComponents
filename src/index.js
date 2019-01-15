a892ede19c93277ec9849a35d6bde665a83e400a
Kacper Zieliński <zielinski.kac@gmail.com>
2019-01-15 09:45:48 +0100

    Dodanie komentarzy wraz z losowymi użytkownikami w brzydki sposób

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Faker from 'faker';
 
 const App = () => {
 	return (
 		<div className="ui container comments">
 			<div className="comment">
-				<a href="#" className="avatar">
-					<img alt="avatar" />
+				<a href="/" className="avatar">
+					<img alt="avatar" src={Faker.image.avatar()} />
 				</a>
 				<div className="content">
-					<a href="#" className="author">
-						Kacper
+					<a href="/" className="author">
+						{Faker.name.firstName()}
+					</a>
+					<div className="metadata">
+						<span className="date">Dzisiaj o 6:00</span>
+					</div>
+					<div className="text">Jakiś przykładowy wpis.</div>
+				</div>
+			</div>
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" src={Faker.image.avatar()} />
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						{Faker.name.firstName()}
+					</a>
+					<div className="metadata">
+						<span className="date">Dzisiaj o 6:00</span>
+					</div>
+					<div className="text">Jakiś przykładowy wpis.</div>
+				</div>
+			</div>
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" src={Faker.image.avatar()} />
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						{Faker.name.firstName()}
 					</a>
 					<div className="metadata">
 						<span className="date">Dzisiaj o 6:00</span>
