785777980a8d02dd9b7aa66fd2a77e8661798541
Kacper Zieliński <kacper.zielinski@wat.edu.pl>
2019-01-15 18:23:38 +0100

    Umieszczono Komentarz wewnątrz Karty do zatwierdzania

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
 import Faker from "faker/locale/pl";
 
-const ApprovalCard = () => {
+const ApprovalCard = props => {
   return (
     <div className="ui card">
-      <div className="content">Jesteś pewien?</div>
+      <div className="content">{props.children}</div>
       <div className="extra content">
         <div className="ui two buttons">
           <div className="ui basic green button">Zatwierdź</div>
--- src/index.js
+++ src/index.js
@@ -7,25 +7,31 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        autor="Wójek Sam"
-        kiedySkomentowano="Dzisiaj o 6:00"
-        textKomentarza={Faker.lorem.sentence()}
-        avatar={Faker.image.avatar()}
-      />
-      <CommentDetail
-        autor={Faker.name.firstName()}
-        kiedySkomentowano="Dzisiaj o 11:00"
-        textKomentarza={Faker.lorem.sentence()}
-        avatar={Faker.image.avatar()}
-      />
-      <CommentDetail
-        autor={Faker.name.firstName()}
-        kiedySkomentowano="Wczoraj o 14:00"
-        textKomentarza={Faker.lorem.sentence()}
-        avatar={Faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          autor="Wójek Sam"
+          kiedySkomentowano="Dzisiaj o 6:00"
+          textKomentarza={Faker.lorem.sentence()}
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          autor={Faker.name.firstName()}
+          kiedySkomentowano="Dzisiaj o 11:00"
+          textKomentarza={Faker.lorem.sentence()}
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          autor={Faker.name.firstName()}
+          kiedySkomentowano="Wczoraj o 14:00"
+          textKomentarza={Faker.lorem.sentence()}
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>Jesteś pewien?</ApprovalCard>
     </div>
   );
 };
