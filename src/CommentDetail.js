18df620dee436bfcc148eb1dc1d4c73617dc720a
Kacper Zieliński <zielinski.kac@gmail.com>
2019-01-15 10:48:28 +0100

    Dodane przekazywanie informacji do komponentu

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Faker from 'faker/locale/pl';
+//import ReactDOM from 'react-dom';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
 	return (
 		<div className="comment">
 			<a href="/" className="avatar">
-				<img alt="avatar" src={Faker.image.avatar()} />
+				<img alt="avatar" src={props.avatar} />
 			</a>
 			<div className="content">
 				<a href="/" className="author">
-					{Faker.name.firstName()}
+					{props.autor}
 				</a>
 				<div className="metadata">
-					<span className="date">Dzisiaj o 6:00</span>
+					<span className="date">{props.kiedySkomentowano}</span>
 				</div>
-				<div className="text">Jakiś przykładowy wpis.</div>
+				<div className="text">{props.textKomentarza}</div>
 			</div>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import Faker from 'faker/locale/pl';
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail />
-			<CommentDetail />
-			<CommentDetail />
+			<CommentDetail
+				autor="Wójek Sam"
+				kiedySkomentowano="Dzisiaj o 6:00"
+				textKomentarza={Faker.lorem.sentence()}
+				avatar={Faker.image.avatar()}
+			/>
+			<CommentDetail
+				autor={Faker.name.firstName()}
+				kiedySkomentowano="Dzisiaj o 11:00"
+				textKomentarza={Faker.lorem.sentence()}
+				avatar={Faker.image.avatar()}
+			/>
+			<CommentDetail
+				autor={Faker.name.firstName()}
+				kiedySkomentowano="Wczoraj o 14:00"
+				textKomentarza={Faker.lorem.sentence()}
+				avatar={Faker.image.avatar()}
+			/>
 		</div>
 	);
 };
