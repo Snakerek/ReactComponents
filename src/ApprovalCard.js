a475d48223fe771d1a28615affc742e1e0d9abe6
Kacper Zieliński <zielinski.kac@gmail.com>
2019-01-16 10:32:29 +0100

    Zakomentowane niepotrzebne importy

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
-import Faker from "faker/locale/pl";
+import React from 'react';
+//import ReactDOM from "react-dom";
+//import CommentDetail from "./CommentDetail";
+//import Faker from "faker/locale/pl";
 
-const ApprovalCard = props => {
-  return (
-    <div className="ui card">
-      <div className="content">{props.children}</div>
-      <div className="extra content">
-        <div className="ui two buttons">
-          <div className="ui basic green button">Zatwierdź</div>
-          <div className="ui basic red button">Odrzuć</div>
-        </div>
-      </div>
-    </div>
-  );
+const ApprovalCard = (props) => {
+	return (
+		<div className="ui card">
+			<div className="content">{props.children}</div>
+			<div className="extra content">
+				<div className="ui two buttons">
+					<div className="ui basic green button">Zatwierdź</div>
+					<div className="ui basic red button">Odrzuć</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ApprovalCard;
